dc7ea52419f7ba222d70e59e99f59e14ca2c23f3
adzkarfauzie <adzkarfauzie02@gmail.com>
2018-06-16 05:32:42 +0700

    Update DB Connection

--- api/models/connection.js
+++ api/models/connection.js
@@ -1,7 +1,7 @@
 module.exports = function(db) {
   // db
   db.Promise = global.Promise;
-  db.connect('mongodb://<dburl>', (err) => {
+  db.connect(process.env.MONGODB_URI, (err) => {
     if(err)
       console.log('Error: ',err);
     console.log('Conected');
